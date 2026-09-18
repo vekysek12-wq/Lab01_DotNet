f3c9ddbf71782ffce99b0eb168c4d13372bfe452
vekys <vekysek12@gmail.com>
2026-09-18 16:53:07 +0300

    Step 8: arithmetic and calculator

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -16,32 +16,61 @@
 //     }
 // }
     
-using System;
+// using System;
 
-class Program
-{
-    static void Main()
-    {
-        string firstName = "Дмитрий";
-        string lastName = "Сташенко";
-        string group = "ИСП-243";
-        int birthYear = 2008;
-        double gpa = 4.2;
-        bool hasScholarship = false;
+// class Program
+// {
+//     static void Main()
+//     {
+//         string firstName = "Дмитрий";
+//         string lastName = "Сташенко";
+//         string group = "ИСП-243";
+//         int birthYear = 2008;
+//         double gpa = 4.2;
+//         bool hasScholarship = false;
 
    
-        int currentYear = 2026;
-        int age = currentYear - birthYear;
+//         int currentYear = 2026;
+//         int age = currentYear - birthYear;
 
-        Console.WriteLine("Студенческое удостоверение");
-        Console.WriteLine($"Имя: {firstName} {lastName}");
-        Console.WriteLine($"Группа: {group}");
-        Console.WriteLine($"Возраст: {age} лет");
-        Console.WriteLine($"Средний балл: {gpa}");
-        Console.WriteLine($"Стипендия: {hasScholarship}");
+//         Console.WriteLine("Студенческое удостоверение");
+//         Console.WriteLine($"Имя: {firstName} {lastName}");
+//         Console.WriteLine($"Группа: {group}");
+//         Console.WriteLine($"Возраст: {age} лет");
+//         Console.WriteLine($"Средний балл: {gpa}");
+//         Console.WriteLine($"Стипендия: {hasScholarship}");
 
-        Console.Write("\nВведите ваш любимый предмет: ");
-        string subject = Console.ReadLine();
-        Console.WriteLine($"Отлично! {firstName} любит {subject}.");
-    }
-}
+//         Console.Write("\nВведите ваш любимый предмет: ");
+//         string subject = Console.ReadLine();
+//         Console.WriteLine($"Отлично! {firstName} любит {subject}.");
+//     }
+// }
+        int a = 15;
+        int b = 4;
+
+        
+        Console.WriteLine($"Сумма: {a + b}");
+        Console.WriteLine($"Разность: {a - b}");
+        Console.WriteLine($"Произведение: {a * b}");
+        Console.WriteLine($"Частное (int): {a / b}");
+        Console.WriteLine($"Остаток: {a % b}");
+
+        
+        double result = (double)a / b;
+        Console.WriteLine($"Частное (double): {result}");
+        
+        Console.WriteLine("Калькулятор");
+        Console.Write("Введите первое число: ");
+        double num1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        double num2 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Сумма: {num1 + num2}");
+        Console.WriteLine($"Разность: {num1 - num2}");
+        Console.WriteLine($"Произведение: {num1 * num2}");
+
+        if (num2 != 0)
+            Console.WriteLine($"Частное: {num1 / num2}");
+        else
+            Console.WriteLine("Деление на ноль невозможно!");
